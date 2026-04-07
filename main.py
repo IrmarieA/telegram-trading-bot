c9fa0dd34e06311ca1bd0f1e8a059288624f0b47
IrmarieA <irmarie.alvarez@ymail.com>
2026-04-07 02:10:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import logging
 from aiogram import Bot, Dispatcher
 
 from handlers import router
-from bot.scheduler import setup_scheduler
+from scheduler import setup_scheduler
 from config import TELEGRAM_BOT_TOKEN, TRADER_CHAT_ID
 from storage.db import init_db
 
